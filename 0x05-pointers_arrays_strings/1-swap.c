e5d85a680940ac20f07e966360d8dcb0709c4675
h01nait <h01nait@gmail.com>
2023-03-27 17:45:28 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * swap_int - function that swaps two integer
+ *
+ * @a: pointer to first num address
+ * @b: pointer to second num address
+ * Return: void
+ */
+void   swap_int(int *a, int *b)
+{
+    int	temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
