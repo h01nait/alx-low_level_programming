86c18c5dd4964935e4a776473d59fc3b6bef1f98
h01nait <h01nait@gmail.com>
2023-03-27 17:49:03 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,10 +6,10 @@
  * Return: void
  */
 void   swap_int(int *a, int *b)
-{
-    int	temp;
-   
-    temp = *a;
-    *a = *b;
-    *b = temp;
+{	
+	int	temp;
+	
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
