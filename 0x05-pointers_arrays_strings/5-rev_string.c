b7d3b6868bb01e24a277f73f8ee0d35cbeaf25e6
h01nait <h01nait@gmail.com>
2023-03-27 18:40:37 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverse string
+ *
+ * @s: string to reverse
+ * Return: void
+ */
+void	rev_string(char *s)
+{
+	int	left;
+	int	right;
+	char	c;
+
+	left = 0;
+	right = 0;
+	while (s[right])
+		right++;
+	right -= 1;
+	while (left < right)
+	{
+		c = s[left];
+		s[left] = s[right];
+		s[right] = c;
+		left++;
+		right--;
+	}
+}
