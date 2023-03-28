33772f9eec5565ca821c95f9ceb968c525d733a4
h01nait <h01nait@gmail.com>
2023-03-27 18:46:42 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - prints the second half of the string
+ *
+ * @str: the string prameter
+ * Return: void
+ */
+void	puts_half(char *str)
+{
+	int	len;
+	int	start;
+
+	len = 0;
+	while (str[len])
+		len++;
+	start = len / 2;
+	start = (len % 2) ? start + 1 : start;
+	while (start < len)
+	{
+		_putchar(str[start]);
+		start++;
+	}
+	_putchar('\n');
+
+}
