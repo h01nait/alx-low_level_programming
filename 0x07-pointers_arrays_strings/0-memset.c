5212c663eb04b690b6f3a4fe0938e49fcdebf55a
h01nait <h01nait@gmail.com>
2023-04-03 17:40:47 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+char	*_memset(char *s, char b, unsigned int n)
+{
+	unsigned int	index;
+
+	index = 0;
+	while (index < n)
+	{
+		s[index] = b;
+		index++;
+	}
+	return (s);
+}
