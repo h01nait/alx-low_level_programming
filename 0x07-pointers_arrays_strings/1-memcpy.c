e91ae6bea3d96a9e80c0e5ea633d189b0bba2538
h01nait <h01nait@gmail.com>
2023-04-03 18:03:51 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,20 @@
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * _memcpy - function that copies whatever in source to destination
  *
- * Return: Nothing.
+ * @dest: buffer that will receive whatever in source
+ * @src: buffer to copy the buffer from
+ * @n: size to be copied from src
+ * Return: dest
  */
-char *_memcpy(char *dest, char *src, unsigned int n){
-  unsigned int index;
-  index = 0;
-    while (index < n){
+char	*_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	index;
 
-      dest[index]=src[index];
-      index++;
-      
-    }
-  return(dest);
+	index = 0;
+	while (index < n)
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	return (dest);
 }
