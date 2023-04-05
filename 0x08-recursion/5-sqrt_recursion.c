ce72674f14e8e2f4a5a39d27799a23c7989b6ee6
h01nait <h01nait@gmail.com>
2023-04-05 14:50:47 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,7 +2,7 @@
 /**
  *_sqrt_recursion - Calculates the natural square root of a number
  *@n: the number to calculate the square root.
- * Return: the square root of n, or -1 if n does not have a natural square root.
+ * Return: the square root of n, or -1 not have a natural square root.
  */
 int _sqrt_recursion(int n)
 {
