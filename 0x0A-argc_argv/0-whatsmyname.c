449282e13ed9d3caf4d50d10678bc66e1b6f15d0
h01nait <h01nait@gmail.com>
2023-04-10 16:53:30 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the program
+ *
+ * @argc: length of the arguments
+ * @argv: array contains agruments
+ * Return: Success 0
+ */
+int	main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
